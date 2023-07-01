0fe924bed1ef9b22b1c6f45b6e96a79c59bb0cd1
João Mauro Siqueira <emaildojoaomauro@gmail.com>
2023-07-01 09:39:11 -0300

    update-main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //1 Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
 
-console.log('Hello World')
+alert("Hello World!")
 
 // ---------------------------------------------------------
 
